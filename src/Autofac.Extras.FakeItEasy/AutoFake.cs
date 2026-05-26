3aa8d1792fdedf095389bccd0732fe38e40d32ab
Travis Illig <tillig@paraesthesia.com>
2026-05-26 11:28:26 -0700

    Standardize build metadata and code formatting

--- src/Autofac.Extras.FakeItEasy/AutoFake.cs
+++ src/Autofac.Extras.FakeItEasy/AutoFake.cs
@@ -1,5 +1,6 @@
-﻿// Copyright (c) Autofac Project. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+﻿// <copyright file="AutoFake.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 using System;
 using System.Collections.Generic;
@@ -8,153 +9,160 @@ using System.Security;
 using Autofac.Core;
 using Autofac.Features.ResolveAnything;
 
-namespace Autofac.Extras.FakeItEasy
+namespace Autofac.Extras.FakeItEasy;
+
+/// <summary>
+/// Wrapper around <see cref="Autofac"/> and <see cref="FakeItEasy"/>.
+/// </summary>
+[SecurityCritical]
+public class AutoFake : IDisposable
 {
+    private readonly Stack<ILifetimeScope> _scopes = new Stack<ILifetimeScope>();
+
+    private bool _disposed;
+
+    [SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", Justification = "It's only a reference, dispose is called from the _scopes Stack")]
+    private ILifetimeScope _currentScope;
+
     /// <summary>
-    /// Wrapper around <see cref="Autofac"/> and <see cref="FakeItEasy"/>.
+    /// Initializes a new instance of the <see cref="AutoFake" /> class.
     /// </summary>
-    [SecurityCritical]
-    public class AutoFake : IDisposable
+    /// <param name="strict">
+    /// <see langword="true" /> to create strict fakes.
+    /// This means that any calls to the fakes that have not been explicitly configured will throw an exception.
+    /// </param>
+    /// <param name="callsBaseMethods">
+    /// <see langword="true" /> to delegate configured method calls to the base method of the faked method.
+    /// </param>
+    /// <param name="builder">The container builder to use to build the container.</param>
+    /// <param name="configureFake">Specifies an action that should be run over a fake object before it's created.</param>
+    /// <param name="configureAction">Specifies actions that needs to be performed on the container builder, like registering additional services.</param>
+    public AutoFake(
+        bool strict = false,
+        bool callsBaseMethods = false,
+        Action<object> configureFake = null,
+        ContainerBuilder builder = null,
+        Action<ContainerBuilder> configureAction = null)
     {
-        private bool _disposed;
+        builder ??= new ContainerBuilder();
 
-        private readonly Stack<ILifetimeScope> _scopes = new Stack<ILifetimeScope>();
+        builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource().WithRegistrationsAs(b => b.InstancePerLifetimeScope()));
+        builder.RegisterSource(new FakeRegistrationHandler(strict, callsBaseMethods, configureFake));
+        configureAction?.Invoke(builder);
+        this.Container = builder.Build();
+        this._currentScope = this.Container.BeginLifetimeScope();
+    }
 
-        [SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", Justification = "It's only a reference, dispose is called from the _scopes Stack")]
-        private ILifetimeScope _currentScope;
+    /// <summary>
+    /// Finalizes an instance of the <see cref="AutoFake"/> class.
+    /// </summary>
+    [SecuritySafeCritical]
+    ~AutoFake()
+    {
+        this.Dispose(false);
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AutoFake" /> class.
-        /// </summary>
-        /// <param name="strict">
-        /// <see langword="true" /> to create strict fakes.
-        /// This means that any calls to the fakes that have not been explicitly configured will throw an exception.
-        /// </param>
-        /// <param name="callsBaseMethods">
-        /// <see langword="true" /> to delegate configured method calls to the base method of the faked method.
-        /// </param>
-        /// <param name="builder">The container builder to use to build the container.</param>
-        /// <param name="configureFake">Specifies an action that should be run over a fake object before it's created.</param>
-        /// <param name="configureAction">Specifies actions that needs to be performed on the container builder, like registering additional services.</param>
-        public AutoFake(
-            bool strict = false,
-            bool callsBaseMethods = false,
-            Action<object> configureFake = null,
-            ContainerBuilder builder = null,
-            Action<ContainerBuilder> configureAction = null)
+    /// <summary>
+    /// Gets the <see cref="IContainer"/> that handles the component resolution.
+    /// </summary>
+    public IContainer Container
+    {
+        get;
+    }
+
+    /// <summary>
+    /// Disposes internal container.
+    /// </summary>
+    [SecuritySafeCritical]
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Resolve the specified type in the container (register it if needed).
+    /// </summary>
+    /// <typeparam name="T">The type of the service.</typeparam>
+    /// <param name="parameters">Optional parameters.</param>
+    /// <returns>The service.</returns>
+    public T Resolve<T>(params Parameter[] parameters) => this._currentScope.Resolve<T>(parameters);
+
+    /// <summary>
+    /// Resolve the specified type in the container (register it if needed).
+    /// </summary>
+    /// <typeparam name="T">The type of the service.</typeparam>
+    /// <param name="parameters">Optional parameters.</param>
+    /// <returns>The service.</returns>
+    [Obsolete("Use Resolve<T>() instead")]
+    public T Create<T>(params Parameter[] parameters) => this.Resolve<T>(parameters);
+
+    /// <summary>
+    /// Resolve the specified type in the container (register it if needed).
+    /// </summary>
+    /// <typeparam name="TService">The type of the service.</typeparam>
+    /// <typeparam name="TImplementation">The implementation of the service.</typeparam>
+    /// <param name="parameters">Optional parameters.</param>
+    /// <returns>The service.</returns>
+    [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "The component registry is responsible for registration disposal.")]
+    public TService Provide<TService, TImplementation>(params Parameter[] parameters)
+    {
+        var scope = this._currentScope.BeginLifetimeScope(b =>
         {
-            builder ??= new ContainerBuilder();
+            b.RegisterType<TImplementation>().As<TService>().InstancePerLifetimeScope();
+        });
 
-            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource().WithRegistrationsAs(b => b.InstancePerLifetimeScope()));
-            builder.RegisterSource(new FakeRegistrationHandler(strict, callsBaseMethods, configureFake));
-            configureAction?.Invoke(builder);
-            this.Container = builder.Build();
-            this._currentScope = this.Container.BeginLifetimeScope();
-        }
+        this._scopes.Push(scope);
+        this._currentScope = scope;
 
-        /// <summary>
-        /// Finalizes an instance of the <see cref="AutoFake"/> class.
-        /// </summary>
-        [SecuritySafeCritical]
-        ~AutoFake() => this.Dispose(false);
+        return this._currentScope.Resolve<TService>(parameters);
+    }
 
-        /// <summary>
-        /// Gets the <see cref="IContainer"/> that handles the component resolution.
-        /// </summary>
-        public IContainer Container { get; }
-
-        /// <summary>
-        /// Disposes internal container.
-        /// </summary>
-        [SecuritySafeCritical]
-        public void Dispose()
+    /// <summary>
+    /// Resolve the specified type in the container (register specified instance if needed).
+    /// </summary>
+    /// <typeparam name="TService">The type of the service.</typeparam>
+    /// <param name="instance">The instance to register if needed.</param>
+    /// <returns>The instance resolved from container.</returns>
+    [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "The component registry is responsible for registration disposal.")]
+    public TService Provide<TService>(TService instance)
+        where TService : class
+    {
+        var scope = this._currentScope.BeginLifetimeScope(b =>
         {
-            this.Dispose(true);
-            GC.SuppressFinalize(this);
-        }
+            b.Register(c => instance).InstancePerLifetimeScope();
+        });
 
-        /// <summary>
-        /// Resolve the specified type in the container (register it if needed).
-        /// </summary>
-        /// <typeparam name="T">The type of the service.</typeparam>
-        /// <param name="parameters">Optional parameters.</param>
-        /// <returns>The service.</returns>
-        public T Resolve<T>(params Parameter[] parameters) => this._currentScope.Resolve<T>(parameters);
+        this._scopes.Push(scope);
+        this._currentScope = scope;
 
-        /// <summary>
-        /// Resolve the specified type in the container (register it if needed).
-        /// </summary>
-        /// <typeparam name="T">The type of the service.</typeparam>
-        /// <param name="parameters">Optional parameters.</param>
-        /// <returns>The service.</returns>
-        [Obsolete("Use Resolve<T>() instead")]
-        public T Create<T>(params Parameter[] parameters) => this.Resolve<T>(parameters);
+        return this._currentScope.Resolve<TService>();
+    }
 
-        /// <summary>
-        /// Resolve the specified type in the container (register it if needed).
-        /// </summary>
-        /// <typeparam name="TService">The type of the service.</typeparam>
-        /// <typeparam name="TImplementation">The implementation of the service.</typeparam>
-        /// <param name="parameters">Optional parameters.</param>
-        /// <returns>The service.</returns>
-        [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "The component registry is responsible for registration disposal.")]
-        public TService Provide<TService, TImplementation>(params Parameter[] parameters)
+    /// <summary>
+    /// Handles disposal of managed and unmanaged resources.
+    /// </summary>
+    /// <param name="disposing">
+    /// <see langword="true" /> to dispose of managed resources (during a manual execution
+    /// of <see cref="AutoFake.Dispose()"/>); or
+    /// <see langword="false" /> if this is getting run as part of finalization where
+    /// managed resources may have already been cleaned up.
+    /// </param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!this._disposed)
         {
-            var scope = this._currentScope.BeginLifetimeScope(b =>
+            if (disposing)
             {
-                b.RegisterType<TImplementation>().As<TService>().InstancePerLifetimeScope();
-            });
-
-            this._scopes.Push(scope);
-            this._currentScope = scope;
-
-            return this._currentScope.Resolve<TService>(parameters);
-        }
-
-        /// <summary>
-        /// Resolve the specified type in the container (register specified instance if needed).
-        /// </summary>
-        /// <typeparam name="TService">The type of the service.</typeparam>
-        /// <param name="instance">The instance to register if needed.</param>
-        /// <returns>The instance resolved from container.</returns>
-        [SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "The component registry is responsible for registration disposal.")]
-        public TService Provide<TService>(TService instance)
-            where TService : class
-        {
-            var scope = this._currentScope.BeginLifetimeScope(b =>
-            {
-                b.Register(c => instance).InstancePerLifetimeScope();
-            });
-
-            this._scopes.Push(scope);
-            this._currentScope = scope;
-
-            return this._currentScope.Resolve<TService>();
-        }
-
-        /// <summary>
-        /// Handles disposal of managed and unmanaged resources.
-        /// </summary>
-        /// <param name="disposing">
-        /// <see langword="true" /> to dispose of managed resources (during a manual execution
-        /// of <see cref="AutoFake.Dispose()"/>); or
-        /// <see langword="false" /> if this is getting run as part of finalization where
-        /// managed resources may have already been cleaned up.
-        /// </param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!this._disposed)
-            {
-                if (disposing)
+                while (this._scopes.Count > 0)
                 {
-                    while (this._scopes.Count > 0)
-                        this._scopes.Pop().Dispose();
-
-                    this.Container.Dispose();
+                    this._scopes.Pop().Dispose();
                 }
 
-                this._disposed = true;
+                this.Container.Dispose();
             }
+
+            this._disposed = true;
         }
     }
 }
--- test/Autofac.Extras.FakeItEasy.Test/AutoFakeFixture.cs
+++ test/Autofac.Extras.FakeItEasy.Test/AutoFakeFixture.cs
@@ -1,257 +1,266 @@
-﻿// Copyright (c) Autofac Project. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+﻿// <copyright file="AutoFakeFixture.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 using System;
 using FakeItEasy;
 using Xunit;
 
-namespace Autofac.Extras.FakeItEasy.Test
+namespace Autofac.Extras.FakeItEasy.Test;
+
+public class AutoFakeFixture
 {
-    public class AutoFakeFixture
+    public interface IBar
     {
-        public interface IBar
+        bool Gone
         {
-            bool Gone { get; }
-
-            void Go();
-
-            IBar Spawn();
+            get;
         }
 
-        public interface IBaz
+        void Go();
+
+        IBar Spawn();
+    }
+
+    public interface IBaz
+    {
+        void Go();
+    }
+
+    [Fact]
+    public void ByDefaultAbstractTypesAreResolvedToTheSameSharedInstance()
+    {
+        using (var fake = new AutoFake())
         {
-            void Go();
+            var bar1 = fake.Resolve<IBar>();
+            var bar2 = fake.Resolve<IBar>();
+
+            Assert.Same(bar1, bar2);
         }
+    }
 
-        [Fact]
-        public void ByDefaultAbstractTypesAreResolvedToTheSameSharedInstance()
+    [Fact]
+    public void ByDefaultConcreteTypesAreResolvedToTheSameSharedInstance()
+    {
+        using (var fake = new AutoFake())
         {
-            using (var fake = new AutoFake())
-            {
-                var bar1 = fake.Resolve<IBar>();
-                var bar2 = fake.Resolve<IBar>();
+            var baz1 = fake.Resolve<Baz>();
+            var baz2 = fake.Resolve<Baz>();
 
-                Assert.Same(bar1, bar2);
-            }
+            Assert.Same(baz1, baz2);
         }
+    }
 
-        [Fact]
-        public void ByDefaultConcreteTypesAreResolvedToTheSameSharedInstance()
+    [Fact]
+    public void ByDefaultFakesAreNotStrict()
+    {
+        using (var fake = new AutoFake())
         {
-            using (var fake = new AutoFake())
-            {
-                var baz1 = fake.Resolve<Baz>();
-                var baz2 = fake.Resolve<Baz>();
+            var foo = fake.Resolve<Foo>();
 
-                Assert.Same(baz1, baz2);
-            }
+            // Should not throw.
+            foo.Go();
         }
+    }
 
-        [Fact]
-        public void ByDefaultFakesAreNotStrict()
+    [Fact]
+    public void ByDefaultFakesDoNotCallBaseMethods()
+    {
+        using (var fake = new AutoFake())
         {
-            using (var fake = new AutoFake())
-            {
-                var foo = fake.Resolve<Foo>();
-
-                // Should not throw.
-                foo.Go();
-            }
+            var bar = fake.Resolve<Bar>();
+            bar.Go();
+            Assert.False(bar.Gone);
         }
+    }
 
-        [Fact]
-        public void ByDefaultFakesDoNotCallBaseMethods()
+    [Fact]
+    public void ByDefaultFakesRespondToCalls()
+    {
+        using (var fake = new AutoFake())
         {
-            using (var fake = new AutoFake())
-            {
-                var bar = fake.Resolve<Bar>();
-                bar.Go();
-                Assert.False(bar.Gone);
-            }
+            var bar = fake.Resolve<IBar>();
+            var result = bar.Spawn();
+            Assert.NotNull(result);
         }
+    }
 
-        [Fact]
-        public void ByDefaultFakesRespondToCalls()
+    [Fact]
+    public void CanResolveFakesWhichCallsBaseMethods()
+    {
+        using (var fake = new AutoFake(callsBaseMethods: true))
         {
-            using (var fake = new AutoFake())
-            {
-                var bar = fake.Resolve<IBar>();
-                var result = bar.Spawn();
-                Assert.NotNull(result);
-            }
+            var bar = fake.Resolve<Bar>();
+            bar.Go();
+            Assert.True(bar.Gone);
         }
+    }
 
-        [Fact]
-        public void CanResolveFakesWhichCallsBaseMethods()
+    [Fact]
+    public void CanResolveFakesWhichCallsBaseMethodsAndInvokeAbstractMethod()
+    {
+        using (var fake = new AutoFake(callsBaseMethods: true))
         {
-            using (var fake = new AutoFake(callsBaseMethods: true))
-            {
-                var bar = fake.Resolve<Bar>();
-                bar.Go();
-                Assert.True(bar.Gone);
-            }
+            var bar = fake.Resolve<Bar>();
+            bar.GoAbstractly();
         }
+    }
 
-        [Fact]
-        public void CanResolveFakesWhichCallsBaseMethodsAndInvokeAbstractMethod()
+    [Fact]
+    public void CanResolveFakesWhichInvokeActionsWhenResolved()
+    {
+        var resolvedFake = (object)null;
+        using (var fake = new AutoFake(configureFake: obj => resolvedFake = obj))
         {
-            using (var fake = new AutoFake(callsBaseMethods: true))
-            {
-                var bar = fake.Resolve<Bar>();
-                bar.GoAbstractly();
-            }
+            var bar = fake.Resolve<IBar>();
+            Assert.Same(bar, resolvedFake);
         }
+    }
 
-        [Fact]
-        public void CanResolveFakesWhichInvokeActionsWhenResolved()
+    [Fact]
+    public void CanResolveStrictFakes()
+    {
+        using (var fake = new AutoFake(strict: true))
         {
-            var resolvedFake = (object)null;
-            using (var fake = new AutoFake(configureFake: obj => resolvedFake = obj))
-            {
-                var bar = fake.Resolve<IBar>();
-                Assert.Same(bar, resolvedFake);
-            }
+            var foo = fake.Resolve<Foo>();
+            Assert.Throws<ExpectationException>(() => foo.Go());
         }
+    }
 
-        [Fact]
-        public void CanResolveStrictFakes()
+    [Fact]
+    public void ProvidesImplementations()
+    {
+        using (var fake = new AutoFake())
         {
-            using (var fake = new AutoFake(strict: true))
-            {
-                var foo = fake.Resolve<Foo>();
-                Assert.Throws<ExpectationException>(() => foo.Go());
-            }
+            var baz = fake.Provide<IBaz, Baz>();
+
+            Assert.NotNull(baz);
+            Assert.True(baz is Baz);
         }
+    }
 
-        [Fact]
-        public void ProvidesImplementations()
+    [Fact]
+    public void ProvidesInstances()
+    {
+        using (var fake = new AutoFake())
         {
-            using (var fake = new AutoFake())
-            {
-                var baz = fake.Provide<IBaz, Baz>();
+            var bar = A.Fake<IBar>();
+            fake.Provide(bar);
 
-                Assert.NotNull(baz);
-                Assert.True(baz is Baz);
-            }
+            var foo = fake.Resolve<Foo>();
+            foo.Go();
+
+            A.CallTo(() => bar.Go()).MustHaveHappened();
         }
+    }
 
-        [Fact]
-        public void ProvidesInstances()
+    [Fact]
+    public void CallsBaseMethodsOverridesStrict()
+    {
+        // A characterization test, intended to detect accidental changes in behavior.
+        // This is an odd situation, since specifying both strict and callsBaseMethods only makes
+        // sense when there are concrete methods on the fake that we want to be executed, but we
+        // want to reject the invocation of any methods that are left abstract on the faked type.
+        using (var fake = new AutoFake(callsBaseMethods: true, strict: true))
         {
-            using (var fake = new AutoFake())
-            {
-                var bar = A.Fake<IBar>();
-                fake.Provide(bar);
-
-                var foo = fake.Resolve<Foo>();
-                foo.Go();
-
-                A.CallTo(() => bar.Go()).MustHaveHappened();
-            }
+            var bar = fake.Resolve<Bar>();
+            bar.Go();
+            Assert.True(bar.Gone);
         }
+    }
 
-        [Fact]
-        public void CallsBaseMethodsOverridesStrict()
+    [Fact]
+    public void CallsBaseMethodsOverridesConfigureFake()
+    {
+        // A characterization test, intended to detect accidental changes in behavior.
+        // Since callsBaseMethods applies globally and configureFake can affect individual
+        // members, having configureFake override callsBaseMethods may be preferred.
+        using (var fake = new AutoFake(
+            callsBaseMethods: true,
+            configureFake: f => A.CallTo(() => ((Bar)f).Go()).DoesNothing()))
         {
-            // A characterization test, intended to detect accidental changes in behavior.
-            // This is an odd situation, since specifying both strict and callsBaseMethods only makes
-            // sense when there are concrete methods on the fake that we want to be executed, but we
-            // want to reject the invocation of any methods that are left abstract on the faked type.
-            using (var fake = new AutoFake(callsBaseMethods: true, strict: true))
-            {
-                var bar = fake.Resolve<Bar>();
-                bar.Go();
-                Assert.True(bar.Gone);
-            }
+            var bar = fake.Resolve<Bar>();
+            bar.Go();
+            Assert.True(bar.Gone);
         }
+    }
 
-        [Fact]
-        public void CallsBaseMethodsOverridesConfigureFake()
+    [Fact]
+    public void ConfigureFakeOverridesStrict()
+    {
+        using (var fake = new AutoFake(
+            strict: true,
+            configureFake: f => A.CallTo(() => ((Bar)f).Go()).DoesNothing()))
         {
-            // A characterization test, intended to detect accidental changes in behavior.
-            // Since callsBaseMethods applies globally and configureFake can affect individual
-            // members, having configureFake override callsBaseMethods may be preferred.
-            using (var fake = new AutoFake(
-                callsBaseMethods: true,
-                configureFake: f => A.CallTo(() => ((Bar)f).Go()).DoesNothing()))
-            {
-                var bar = fake.Resolve<Bar>();
-                bar.Go();
-                Assert.True(bar.Gone);
-            }
+            var bar = fake.Resolve<Bar>();
+            bar.Go();
         }
+    }
 
-        [Fact]
-        public void ConfigureFakeOverridesStrict()
+    public abstract class Bar : IBar
+    {
+        private bool _gone;
+
+        public bool Gone
         {
-            using (var fake = new AutoFake(
-                strict: true,
-                configureFake: f => A.CallTo(() => ((Bar)f).Go()).DoesNothing()))
+            get
             {
-                var bar = fake.Resolve<Bar>();
-                bar.Go();
-            }
-        }
-
-        public abstract class Bar : IBar
-        {
-            private bool _gone;
-
-            public bool Gone
-            {
-                get { return this._gone; }
-            }
-
-            public virtual void Go()
-            {
-                this._gone = true;
-            }
-
-            public IBar Spawn()
-            {
-                throw new NotImplementedException();
-            }
-
-            public abstract void GoAbstractly();
-        }
-
-        public class Baz : IBaz
-        {
-            private bool _gone;
-
-            public bool Gone
-            {
-                get { return this._gone; }
-            }
-
-            public virtual void Go()
-            {
-                this._gone = true;
+                return this._gone;
             }
         }
 
-        public class Foo
+        public virtual void Go()
         {
-            private readonly IBar _bar;
+            this._gone = true;
+        }
 
-            private readonly IBaz _baz;
+        public IBar Spawn()
+        {
+            throw new NotImplementedException();
+        }
 
-            public Foo(IBar bar, IBaz baz)
+        public abstract void GoAbstractly();
+    }
+
+    public class Baz : IBaz
+    {
+        private bool _gone;
+
+        public bool Gone
+        {
+            get
             {
-                this._bar = bar;
-                this._baz = baz;
-            }
-
-            public virtual void Go()
-            {
-                this._bar.Go();
-                this._baz.Go();
+                return this._gone;
             }
         }
 
-        [AttributeUsage(AttributeTargets.Class)]
-        public class ForTestAttribute : Attribute
+        public virtual void Go()
         {
+            this._gone = true;
         }
+    }
+
+    public class Foo
+    {
+        private readonly IBar _bar;
+
+        private readonly IBaz _baz;
+
+        public Foo(IBar bar, IBaz baz)
+        {
+            this._bar = bar;
+            this._baz = baz;
+        }
+
+        public virtual void Go()
+        {
+            this._bar.Go();
+            this._baz.Go();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ForTestAttribute : Attribute
+    {
     }
 }
